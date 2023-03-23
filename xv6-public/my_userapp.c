b3ae4b8a227892f87fecd16f03aa98584f43eb58
2019044711 <2019044711@hanyang.ac.kr>
2023-03-23 07:04:56 +0000

    init

---
+++ xv6-public/my_userapp.c
@@ -0,0 +1,19 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int
+main(int argc, char *argv[])
+{
+	if (argc <= 1)
+		exit();
+
+	if (strcmp(argv[1], "\"user\"") != 0)
+		exit();
+
+	char *buf = "Hello xv6!";
+	int ret_val;
+	ret_val = myfunction(buf);
+	printf(1, "Return value : 0x%x\n", ret_val);
+	exit();
+};
---
+++ xv6-public/prac_syscall.c
@@ -0,0 +1,22 @@
+// check order of header files
+
+#include "types.h"
+#include "defs.h"
+
+int 
+myfunction(char *str)
+{
+	cprintf("%s\n", str);
+	return 0xABCDABCD;
+}
+
+//Wrapper for my_syscall
+int
+sys_myfunction(void)
+{
+char *str;
+//Decode argument using argstr
+if (argstr(0, &str) < 0)
+	return -1;
+return myfunction(str);
+}
