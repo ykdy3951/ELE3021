384105d2c9d0f0299d7ca1cfc8ade8e6b5417d0e
2019044711 <2019044711@hanyang.ac.kr>
2023-06-05 04:54:18 +0000

    synctest 1

--- project03/sync_test.c
+++ project03/sync_test.c
@@ -42,35 +42,35 @@ main(int argc, char *argv[])
     printf(1, "%d bytes written\n", BUF_PER_FILE * BUFSIZE);
     close(fd);
 
-    printf(1, "2. sync check\n");
-    fd = open("synctestfile2", O_CREATE | O_RDWR);
-    for(i = 0; i < BUF_PER_FILE; i++) {
-      if (i % 100 == 0){
-        printf(1, "%d bytes written\n", i * BUFSIZE);
-      }
-      if ((r = write(fd, data, sizeof(data))) != sizeof(data)){
-        printf(1, "write returned %d : failed\n", r);
-        exit();
-      }
-      if ((old_log = get_log_num()) < 0) {
-        printf(1, "get log num returned %d : failed\n", log_num);
-        exit();
-      } 
+    // printf(1, "2. sync check\n");
+    // fd = open("synctestfile2", O_CREATE | O_RDWR);
+    // for(i = 0; i < BUF_PER_FILE; i++) {
+    //   if (i % 100 == 0){
+    //     printf(1, "%d bytes written\n", i * BUFSIZE);
+    //   }
+    //   if ((r = write(fd, data, sizeof(data))) != sizeof(data)){
+    //     printf(1, "write returned %d : failed\n", r);
+    //     exit();
+    //   }
+    //   if ((old_log = get_log_num()) < 0) {
+    //     printf(1, "get log num returned %d : failed\n", log_num);
+    //     exit();
+    //   } 
 
-      if (sync() == -1) {
-        printf(1, "sync failed\n");
-        exit();
-      }
+    //   if (sync() == -1) {
+    //     printf(1, "sync failed\n");
+    //     exit();
+    //   }
 
-      if ((log_num = get_log_num()) < 0) {
-        printf(1, "get log num returned %d : failed\n", log_num);
-        exit();
-      } 
-      printf(1, "get log num : %d -> %d\n", old_log, log_num);
+    //   if ((log_num = get_log_num()) < 0) {
+    //     printf(1, "get log num returned %d : failed\n", log_num);
+    //     exit();
+    //   } 
+    //   printf(1, "get log num : %d -> %d\n", old_log, log_num);
 
-    }
-    printf(1, "%d bytes written\n", BUF_PER_FILE * BUFSIZE);
-    close(fd);
+    // }
+    // printf(1, "%d bytes written\n", BUF_PER_FILE * BUFSIZE);
+    // close(fd);
 
     exit();
 }
