c378a5faa6a1fe159b99b03ccf45566646a38e59
2019044711 <2019044711@hanyang.ac.kr>
2023-06-10 15:54:28 +0000

    add comment of symbolic link

--- project03/ln.c
+++ project03/ln.c
@@ -10,11 +10,16 @@ main(int argc, char *argv[])
     printf(2, "Usage: ln [-h | -s] old new\n");
     exit();
   }
-  // 
-  if(!strcmp(argv[1], "-h") && !(link(argv[2], argv[3]) < 0))
-    printf(2, "hard link %s %s: failed\n", argv[2], argv[3]);
-  else if(!strcmp(argv[1], "-s") && !(symlink(argv[2], argv[3]) < 0))
-    printf(2, "symbolic link %s %s: failed\n", argv[2], argv[3]);
+  // If argv[1] is "-h" then call (hard) link syscall
+  if(!strcmp(argv[1], "-h")) {
+    if(link(argv[2], argv[3]) < 0)
+      printf(2, "hard link %s %s: failed\n", argv[2], argv[3]);
+  }
+  // If argv[1] is "-s" then call symlink syscalls
+  else if(!strcmp(argv[1], "-s")) {
+    if(symlink(argv[2], argv[3]) < 0)
+      printf(2, "symbolic link %s %s: failed\n", argv[2], argv[3]);
+  }
   else
     printf(2, "Usage: ln [-h | -s] old new\n");
   exit();
